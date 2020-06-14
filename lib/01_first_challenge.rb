63bd6f22693ecb2f4de126fd9d3a554ff65e5465
Emily Braman <embraman10@gmail.com>
2020-06-14 16:04:32 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,16 @@
 def first_challenge
-   epic_tragedy = {}
+  epic_tragedy = {
+     :montague => {
+       :patriarch => {:name "Lord Montague", :age => "54"},
+       :matriarch => {},
+       :hero => {},
+       :hero_friends => []
+      },
+      :capulet => {
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
+      }
+ }
 end
